4845a523988990be2ccae3d2c9a2222640ceef7f
Ryan Hetherington <hetheringtonr2@comcast.net>
2021-01-04 13:14:10 -0800

    Made changes

--- main.js
+++ main.js
@@ -3,12 +3,15 @@
 const container = document.querySelector("#container");
 // Create a variable that is equal to the reset button //
 const reset = document.querySelector("#reset");
+// Create a variable that is equal to the grey scale button //
+const grey = document.querySelector("#grey");
 // Create a global variable that will be equal to the number of square divs entered in prompt //
 let gridNumber;
 // Create a global variable that will be equal to every div with a class of 'box' //
 let gridBoxes;
 // Create a function that turns container into a grid and creates an equal number //
 // of rows and columns that are the same number entered in prompt //
+
 function createGrid(gridNumber) {
     container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
@@ -23,6 +26,8 @@ function createGrid(gridNumber) {
     // let gridBoxes turn 'box' into an array //
     // Create a forEach function that will look for every 'box'//
     // Create an event listner that will mouseover every 'box' //
+    // Style the background color into random colors //
+    // style the opacity to darken //
     gridBoxes = document.querySelectorAll(".box");
     gridBoxes.forEach( function(gridBox) {
         gridBox.addEventListener('mouseover', function() {
@@ -30,15 +35,58 @@ function createGrid(gridNumber) {
             let y = Math.floor(Math.random() * 256);
             let z = Math.floor(Math.random() * 256);
             let randomColor = 'rgb(' + x + ', ' + y + ', ' + z + ')';
-            gridBox.style.opacity = (parseFloat(gridBox.style.opacity) || 0) + 0.2;
             gridBox.style.backgroundColor = randomColor;
+            gridBox.style.opacity = (parseFloat(gridBox.style.opacity) || 0) + 0.2;
         });
     });
 }
 
+// This time it's black // 
+function createGrey(gridNumber) {
+    container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+    // Create a for loop that will create divs with the class 'box' //
+    // Append div as child to container //
+    // For loop will iterate through gridnumber twice to represent two dimensions //
+    for (let i = 0; i < gridNumber * gridNumber; i++) {
+        let div = document.createElement('div');
+        div.classList.add('box');
+        container.appendChild(div);
+    }
+    // let gridBoxes turn 'box' into an array //
+    // Create a forEach function that will look for every 'box'//
+    // Create an event listner that will mouseover every 'box' //
+    // Style the background color to black //
+    // Style the opacity to darken //
+    gridBoxes = document.querySelectorAll(".box");
+    gridBoxes.forEach( function(gridBox) {
+        gridBox.addEventListener('mouseover', function() {
+            gridBox.style.backgroundColor = "black";
+            gridBox.style.opacity = (parseFloat(gridBox.style.opacity) || 0) + 0.2;
+        });
+    });
+}
+
+// Create an event listener for grey scale button //
+// gridBoxes forEach() gridBox remove child from container //
+grey.addEventListener( 'click', function() {
+    isClicked = false;
+    gridBoxes.forEach( function (gridBox) {
+        container.removeChild(gridBox);
+    });
+    // Within the event listener run the prompt and call createGrid() //
+    gridNumber = prompt("Enter the numbers for the grid.", 16);
+    if (parseInt(gridNumber) > 100) {
+        alert("Please enter a number less than 100.");
+        return gridNumber;
+    }
+    createGrey(gridNumber);
+});
+createGrey(gridNumber);
 // Create an event listener for reset button //
-// gridboxes forEach() gridbox remove child from container //
+// gridBoxes forEach() gridBox remove child from container //
 reset.addEventListener( 'click', function() {
+    isClicked = false;
     gridBoxes.forEach( function (gridBox) {
         container.removeChild(gridBox);
     });
